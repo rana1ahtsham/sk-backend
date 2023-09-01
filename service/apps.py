e4ad88e1714ae75691bb1117b150fdafd6b0aef2
Ahsan Baryar <lal.ahsan.baryar@gmail.com>
2023-09-01 05:40:41 -0700

    load_csv

--- service/apps.py
+++ service/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class ProviderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'service'
+
+    def ready(self):
+        call_command("makemigrations")
+        call_command("migrate")
+        call_command("load_csv")
